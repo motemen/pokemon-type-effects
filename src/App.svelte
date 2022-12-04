1e7355097bcc07e118425c29ecf0ac472838db6d
Hironao OTSUBO <motemen@gmail.com>
2022-12-04 11:11:57 +0900

    working

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
-  let defenderTypes: [number, number | null] = [0, null];
-
-  let openDialog = false;
-  let dialogFocusIndex = 0;
-
   import Dialog from "@smui/dialog";
+
+  let defenderTypes: [number, number | null] = [0, null];
 
   const TYPES = [
     "normal",
@@ -26,6 +23,30 @@
     "steel",
     "fairy",
   ] as const;
+
+  const params = new URLSearchParams(location.search);
+  const typeFromParams = [...(params.get("d") || "").split(","), null, null]
+    .slice(0, 2)
+    .map((t) => {
+      const i = TYPES.indexOf(t as unknown as TypeName);
+      return i === -1 ? null : i;
+    });
+  if (typeFromParams[0] !== null) {
+    defenderTypes = typeFromParams as [number, number | null];
+  }
+
+  $: {
+    history.pushState(
+      {},
+      "",
+      `?d=${defenderTypes
+        .map((t) => (t === null ? "" : TYPES[t]))
+        .join(",")}`.replace(/,$/, "")
+    );
+  }
+
+  let openDialog = false;
+  let dialogFocusIndex = 0;
 
   type TypeName = typeof TYPES[number];
 
@@ -89,7 +110,7 @@
 <main class="p-4 flex justify-center">
   <div class="w-full max-w-md">
     <section class="w-full">
-      <h1 class="text-xl py-2 mb-3">相手ポケモンのタイプ</h1>
+      <h1 class="text-xl py-2 mt-3 mb-1">相手ポケモンのタイプ</h1>
       <div class="flex gap-4 justify-between">
         {#each defenderTypes as type, i}
           <button
@@ -99,7 +120,7 @@
             }}
             class="ring-1 ring-offset-1 ring-gray-200 flex-1 text-center transition duration-300 bg-type-{TYPES[
               type
-            ]}-300 rounded text-lg px-4 py-2"
+            ]} rounded text-lg px-4 py-2"
             >{TYPE_NAME_AS_JA[TYPES[type]] || "-"}</button
           >
         {/each}
@@ -109,7 +130,7 @@
       <div class="grid grid-cols-2 gap-3 p-6">
         {#each TYPES as type, i}
           <button
-            class="text-center bg-type-{type}-300 px-4 py-2 rounded w-32"
+            class="text-center bg-type-{type} px-4 py-2 rounded w-32"
             on:click={() => {
               openDialog = false;
               defenderTypes[dialogFocusIndex] = i;
@@ -125,15 +146,37 @@
     </Dialog>
 
     <section class="w-full mt-4">
-      <h1 class="text-xl py-2 mb-3">わざのタイプ別効果倍率</h1>
-      {#each effectRatios as ratio}
-        {#if ratio.ratio !== 1.0}
-          <div class="bg-type-{ratio.type}-300 p-3 flex">
-            <span class="grow">{TYPE_NAME_AS_JA[ratio.type]}</span>
-            <span class="w-8">× {ratio.ratio === 0.5 ? "½" : ratio.ratio}</span>
+      <h1 class="text-xl py-2 mt-3 mb-1">わざのタイプ別効果倍率</h1>
+      {#each effectRatios as { ratio, type }}
+        {#if ratio !== 1.0}
+          <div class="bg-type-{type} p-3 flex">
+            <span class="grow">{TYPE_NAME_AS_JA[type]}</span>
+            <span class="w-8"
+              >× {ratio === 0.25 ? "¼" : ratio === 0.5 ? "½" : ratio}</span
+            >
           </div>
         {/if}
       {/each}
+    </section>
+
+    <section class="w-full">
+      <h1 class="text-xl py-2 mt-5 mb-1">結果をツイートする</h1>
+      <a
+        href="https://twitter.com/intent/tweet"
+        class="twitter-share-button"
+        data-size="large"
+        data-lang="ja"
+        data-hashtags="ポケモン"
+        data-text="{defenderTypes
+          .map((type) => TYPE_NAME_AS_JA[TYPES[type]])
+          .filter((x) => x)
+          .join('／')}タイプに対するわざの相性"
+        data-show-count="false">Tweet</a
+      ><script
+        async
+        src="https://platform.twitter.com/widgets.js"
+        charset="utf-8"
+      ></script>
     </section>
   </div>
 </main>
